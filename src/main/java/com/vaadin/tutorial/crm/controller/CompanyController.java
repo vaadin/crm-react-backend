75900cff10f727023daa086589b6b33fc103ea7b
webking-dev <sunmandev@gmail.com>
2020-12-23 05:03:32 -0500

    feat: add CRUD endpoints for company

--- src/main/java/com/vaadin/tutorial/crm/controller/CompanyController.java
+++ src/main/java/com/vaadin/tutorial/crm/controller/CompanyController.java
@@ -4,9 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vaadin.tutorial.crm.entity.Company;
 import com.vaadin.tutorial.crm.service.CompanyService;
 
 @RestController
@@ -22,5 +28,20 @@ public class CompanyController {
   @GetMapping("/companies")
   public ResponseEntity<Object> getCompanies(@RequestParam(required=false, value="search") String search) {
     return new ResponseEntity<>(companyService.findAll(search), HttpStatus.OK);
+  }
+
+  @PostMapping("/company")
+  public void addCompany(@RequestBody Company company) {
+    companyService.save(company);
+  }
+
+  @PutMapping("/company/{id}")
+  public void updateCompany(@PathVariable(value="id") Long id, @RequestBody Company company) {
+    companyService.update(id, company);
+  }
+
+  @DeleteMapping("/company/{id}")
+  public void deleteCompany(@PathVariable(value="id") Long id) {
+    companyService.delete(id);
   }
 }
--- src/main/java/com/vaadin/tutorial/crm/service/CompanyService.java
+++ src/main/java/com/vaadin/tutorial/crm/service/CompanyService.java
@@ -6,11 +6,15 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.Map;
 
 @Service
 public class CompanyService {
 
+    private static final Logger LOGGER = Logger.getLogger(CompanyService.class.getName());
     private CompanyRepository companyRepository;
 
     public CompanyService(CompanyRepository companyRepository) {
@@ -34,5 +38,37 @@ public class CompanyService {
         findAll().forEach(company ->
             stats.put(company.getName(), company.getEmployees().size()));
         return stats;
+    }
+
+    public void delete(Long id) {
+        Optional<Company> opt = companyRepository.findById(id);
+
+        if (opt.isPresent()) {
+            Company company = opt.get();
+            companyRepository.delete(company);
+        }
+    }
+
+    public void save(Company company) {
+        if (company == null) {
+            LOGGER.log(Level.SEVERE,
+                "Company is null. Are you sure you have connected your form to the application?");
+            return;
+        }
+        companyRepository.save(company);
+    }
+
+    public void update(Long id, Company c) {
+        Optional<Company> opt = companyRepository.findById(id);
+
+        if (opt.isPresent()) {
+            Company company = opt.get();
+            company.setName(c.getName());
+            company.setCountry(c.getCountry());
+            company.setAddress(c.getAddress());
+            company.setState(c.getState());
+            company.setZipcode(c.getZipcode());
+            companyRepository.save(company);
+        }
     }
 }
